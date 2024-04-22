6c1935da5564682167588409501483a56a6b57e7
Alexis Scobey <19824067+octoturt@users.noreply.github.com>
2024-04-21 20:41:06 -0400

    added some splashes

--- splash.js
+++ splash.js
@@ -31,7 +31,8 @@ const allSplashes = [
 		"https://store.steampowered.com/app/400/Portal/"],
 	["So how are you holding up?<br>Because I'm a potato.",
 		"https://www.youtube.com/watch?v=vsg-i0Gh2Eg"],
-	[":(){:|:& };:","https://en.wikipedia.org/wiki/Fork_bomb"],
+	[":(){:|:& };:",
+		"https://en.wikipedia.org/wiki/Fork_bomb"],
 	["I confess, I admit, I'm impressed.",
 		"https://www.youtube.com/watch?v=3CccPPDe2JU&pp=ygUSdG1iZyBpJ20gaW1wcmVzc2Vk"],
 	["Everyone is crying<br>at the dread hypnotic flying<br>of the bee of the bird of the moth.",
@@ -39,13 +40,21 @@ const allSplashes = [
 	["We cracked open the skulls of our masters and found nothing but flesh and blood",
 		"https://twitter.com/Iinux/status/1472731389405679623"],
 	["Press Erase, ignore the shrill alarms.",
-		"https://www.youtube.com/watch?v=apHV1CaQWOE&pp=ygUKdG1iZyBlcmFzZQ%3D%3D"],
+		"https://music.youtube.com/watch?v=apHV1CaQWOE&pp=ygUKdG1iZyBlcmFzZQ%3D%3D"],
 	["It had been going so well<br>and then I broke my eggshell.",
 		"https://www.youtube.com/watch?v=F71gpyuer2Y&pp=ygURdG1iZyBicm9udG9zYXVydXM%3D"],
 	["sudo rm -rf --no-preserve-root /",
 		"https://en.wikipedia.org/wiki/Rm_(Unix)#Protection_of_the_filesystem_root"],
 	["our hard work<br>by these words guarded<br>please dont steal",
-		"https://www.rcfp.org/wp-content/uploads/imported/20120105_202426_apple_sealing.pdf"]
+		"https://www.rcfp.org/wp-content/uploads/imported/20120105_202426_apple_sealing.pdf"],
+	["effective. Power لُلُصّبُلُلصّبُررً ॣ ॣh ॣ ॣ 冗",
+		"https://en.wikipedia.org/wiki/SpringBoard#%22effective_power%22_bug"],
+	["Life hands you problems<br>Make problemade",
+		"https://music.youtube.com/watch?v=E6Yk_tlo3XA"],
+	["Protect Trans Kids",
+		"https://www.thetrevorproject.org/"],
+	["&aelig;&ndash;&Dagger;&aring;&shy;&mdash;&aring;&OElig;&ndash;&atilde;&lsquo;",
+		"https://en.wikipedia.org/wiki/Mojibake"]
 ]
 
 document.onload = setSplash();
